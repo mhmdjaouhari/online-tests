1536d4e762b7baf51f2c1824e7c484fbf92495d0
EL KTIBI EL HOUSSINE <elktibi.elhoussine@gmail.com>
2020-01-05 23:57:12 +0100

    test

--- src/server/Main.java
+++ src/server/Main.java
@@ -4,4 +4,5 @@ package server;
 // this will be a singleton class that will maintain the server and will handle client connections
 // it will contain the switch that will dispatch requests to the methods defined in the other classes of the server package (Student, Test...)
 public class Main {
+  // test
 }
