fd6db391e05211a75bb0e5d08f9a79e71d1d6908
mojaouhari <jaouhari1998@gmail.com>
2020-01-17 22:32:43 +0100

    Add ProfesseurDispatcher

---
+++ src/server/dispatchers/ProfesseurDispatcher.java
@@ -0,0 +1,29 @@
+package server.dispatchers;
+
+import models.Etudiant;
+import server.DAOs.EtudiantDAO;
+import util.Action;
+import util.Request;
+import util.Response;
+
+public class ProfesseurDispatcher {
+
+    public static Response handle(Request request){
+        Action action = request.getAction();
+        Etudiant etudiant = (Etudiant) request.getData();
+        Response response;
+        switch (action) {
+            case LOGIN:
+                response = EtudiantDAO.login(etudiant);
+                break;
+            case EXIT:
+                System.out.println("Closing session...");
+                response = new Response(0,"Closing session...");
+                break;
+            default:
+                System.out.println("Action not found");
+                response = new Response(0,"Action not found");
+        }
+        return response;
+    }
+}
