25af069acbdfc013a7f028a48a4ee44bb501ab8c
mojaouhari <jaouhari1998@gmail.com>
2020-02-03 18:33:01 +0100

    Verify server branch merge

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module online.tests {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+    requires com.jfoenix;
+
+    opens GUI.admin;
+    opens GUI.etudiant;
+    opens GUI.professeur;
+    opens GUI.professeur.dashboard;
+    opens models;
+}
