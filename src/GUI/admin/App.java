793ab823b140debac80bae1e66e55e442d167a74
zbakh98 <42416143+zbakh98@users.noreply.github.com>
2020-02-06 19:30:39 +0100

    Update App.java

--- src/GUI/admin/App.java
+++ src/GUI/admin/App.java
@@ -59,7 +59,6 @@ public class App extends Application {
         stage.setScene(new Scene(root));
         stage.centerOnScreen();
         stage.setOnCloseRequest(windowEvent -> {});
-        //stage.setMaximized(true);
         stage.show();
     }
 }
