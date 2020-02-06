45fe4063fec053488f1b6950027d1e350e01ba41
zbakh98 <42416143+zbakh98@users.noreply.github.com>
2020-02-06 19:41:15 +0100

    Update Main.java

--- src/client/Main.java
+++ src/client/Main.java
@@ -8,25 +8,5 @@ public class Main {
     public static void main(String[] args) {
         Client client = new Client(Role.ETUDIANT);
         client.connect();
-//        String username, password;
-//        Scanner scanner = new Scanner(System.in);
-//        Etudiant etudiant = new Etudiant();
-//        do {
-//            username = scanner.nextLine();
-//            if (username.equals("exit")) {
-//                if(client.getEmitter().exit()){
-//                    client.getEmitter().setClientOnline(false);
-//                    break;
-//                }
-//            }
-//            password = scanner.nextLine();
-//            etudiant.setUsername(username);
-//            etudiant.setPassword(password);
-//            try {
-//                ((EtudiantActionEmitter)client.getEmitter()).login(etudiant);
-//            } catch(Exception e){
-//                System.err.print(e.getMessage());
-//            }
-//        } while (true);
     }
 }
