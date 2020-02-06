a175d56d3855c0d028359f687db4a5f2515499fc
zbakh98 <42416143+zbakh98@users.noreply.github.com>
2020-02-06 19:53:17 +0100

    Update Server.java

--- src/server/Server.java
+++ src/server/Server.java
@@ -23,8 +23,6 @@ public class  Server implements Runnable{
                 }
                 socket = serverSocket.accept();
                 System.out.println("Client connected"+" info : "+socket);
-                //ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-                //ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
                 Session session = new Session(socket);
                 session.start();
             }
